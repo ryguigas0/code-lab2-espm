71495dc38b6f1d4ba57158bda8b109d43cc8aff7
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-03-13 15:51:04 -0300

    Finished requisites 1 and 4

--- src/codelab/aula4/pacientes/Main.java
+++ src/codelab/aula4/pacientes/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner kb = new Scanner(System.in);
+        ListaDuplaGenerica<Paciente> pacientes = new ListaDuplaGenerica<Paciente>();
         boolean alive = true;
 
         System.out.println("Seja bem vindo ao Programa de Registros de Paciente (PRP)!");
@@ -30,6 +31,34 @@ public class Main {
                     System.out.println("find -> Encontrar um paciente");
                     System.out.println("delete -> Remover um paciente");
                     System.out.println("list -> Listar todos os pacientes salvos");
+                    break;
+                case "create":
+                    System.out.print("Digite o nome do paciente: ");
+                    String nome = kb.nextLine();
+
+                    System.out.print("Digite a idade do paciente: ");
+                    int idade = kb.nextInt();
+
+                    System.out.print("Digite o peso do paciente, em kg: ");
+                    double pesoKG = kb.nextDouble();
+
+                    System.out.print("Digite a altura do paciente, em metros: ");
+                    double alturaMetros = kb.nextDouble();
+
+                    System.out.print("Digite a circunferência do quadril do paciente, em centimetros: ");
+                    double circunfQuadrilCentimetros = kb.nextDouble();
+
+                    kb.nextLine();
+
+                    pacientes.inserirInicio(new Paciente(nome, idade,
+                            pesoKG, alturaMetros, circunfQuadrilCentimetros));
+
+                    System.out.println("Paciente novo inserido!");
+                    break;
+                case "list":
+                    System.out.println("Lista de pacientes registrados");
+                    System.out.println(pacientes);
+                    break;
                 default:
                     System.out
                             .println("Comando não reconhecido! Escreva `help` para uma lista de comandos disponíveis");
---
+++ src/codelab/aula4/pacientes/Paciente.java
@@ -0,0 +1,43 @@
+package codelab.aula4.pacientes;
+
+public class Paciente {
+    private String nome;
+    private int idade;
+    private double pesoKG;
+    private double alturaMetros;
+    private double circunfQuadrilCentimetros;
+
+    public Paciente(String nome, int idade, double pesoKG, double alturaMetros, double circunfQuadrilCentimetros) {
+        this.nome = nome;
+        this.idade = idade;
+        this.pesoKG = pesoKG;
+        this.alturaMetros = alturaMetros;
+        this.circunfQuadrilCentimetros = circunfQuadrilCentimetros;
+    }
+
+    public double[] freqCardiacaAlvo() {
+        int freqCardiacaMax = 220 - this.idade;
+
+        double[] freqCardiAlvo = new double[2];
+
+        freqCardiAlvo[0] = 0.5 * freqCardiacaMax;
+        freqCardiAlvo[1] = 0.85 * freqCardiacaMax;
+
+        return freqCardiAlvo;
+    }
+
+    public double imc() {
+        return this.pesoKG / Math.pow(alturaMetros, 2);
+    }
+
+    public double bai() {
+        return (this.circunfQuadrilCentimetros / Math.pow(alturaMetros, 3 / 2)) - 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente [nome=" + nome + ", idade=" + idade + ", pesoKG=" + pesoKG + ", alturaMetros=" + alturaMetros
+                + ", circunfQuadrilCentimetros=" + circunfQuadrilCentimetros + "]";
+    }
+
+}
